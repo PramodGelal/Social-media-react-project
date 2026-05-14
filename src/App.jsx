8a41a3595b0f508f25f75035c424c536d414363f
Pramod Gelal <gelalpramod0@gmail.com>
2026-05-14 11:45:14 +0545

    structure is set

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import Header from "./component/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Sidebar } from "./component/Sidebar";
+import { Footer } from "./component/Footer";
+import "./App.css";
+
 function App(){
 
-  return (<>
+  return (<div className="app-container ">
+
+  <Sidebar></Sidebar>
+  <div className="main-container">
   <Header></Header>
+  <Footer></Footer>
+    </div>
   
-  </>);
+  </div>);
 }
 export default App;
