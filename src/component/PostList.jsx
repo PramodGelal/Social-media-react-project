708611bb4016ff12fe4909dab04449e18c1638d1
Pramod Gelal <gelalpramod0@gmail.com>
2026-05-15 10:40:58 +0545

    fetching from server with button click

--- src/component/PostList.jsx
+++ src/component/PostList.jsx
@@ -2,12 +2,23 @@ import { useContext } from "react";
 import { Card } from "./Card";
 import { PostList as PostListData}from "../store/post-list-store";
 import "../App.css";
+import { Welcome } from "./Welcome";
 
 export function PostList(){
-   const {postList}= useContext(PostListData)
+   const { postList, addPosts }= useContext(PostListData);
+   const handelerOnClick=()=>{
+      fetch("https://dummyjson.com/posts")
+      .then((resp)=>resp.json())
+         .then((data)=>{addPosts(data.posts)});
+      
+
+   }
 
     return (
+
 <div className="container-card">
+          {postList.length === 0 && (<Welcome getPost={handelerOnClick}></Welcome>)}
+  
 {postList.map((postdata)=>{
            return <Card key={postdata.id} post={postdata}></Card>;
 
